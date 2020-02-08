1d419f85c21f1648056839ba53e99e84dd1a8e74
Kulcsár Rudolf <kulcsarrudolf@gmail.com>
2020-02-08 18:48:26 +0200

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,10 +2,10 @@ public class Sum {
     public static void main (String[] args) {
         int sum = 0;
 		
-		for (String s: args) {
+	for (String s: args) {
             sum += Integer.parseInt(s);
         }
 		
-		System.out.println("Sum: " + sum);
+	System.out.println("Sum: " + sum);
     }
 }
